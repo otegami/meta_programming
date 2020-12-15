0694de36e8f394f733fb14826c6e757f4e94c575
otegami <a.s.takuya1026@gmail.com>
2020-12-15 21:57:24 +0900

    the difference between proc and lambda

---
+++ blocks/proc_vs_lambda.rb
@@ -0,0 +1,21 @@
+# def double(callable_object)
+#   callable_object.call * 2
+# end
+
+# l = lambda { return 10 }
+# p double(l)
+
+# def another_double
+#   p = Proc.new { return 10 }
+#   result = [p.call]
+#   return result * 2
+# end
+
+# p another_double
+
+def double(callable_object)
+  callable_object.call * 2
+end
+
+proc = Proc.new { return 10 }
+p double(proc)
